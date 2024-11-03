037fec2b0d31fe2ffeae204ce7cf1d9aedd76b35
João Victor Oliveira Couto <commits@jictyvoo.dev>
2024-11-03 15:41:08 -0300

    :open_file_folder: refactor: Rename image utils package

--- internal/imageparser/step_rescale_test.go
+++ internal/imageparser/step_rescale_test.go
@@ -6,12 +6,12 @@ import (
 	"testing"
 
 	"github.com/Jictyvoo/comic_manga-extractor-Converter/internal/deviceprof"
-	"github.com/Jictyvoo/comic_manga-extractor-Converter/internal/utils/imgcompare"
+	"github.com/Jictyvoo/comic_manga-extractor-Converter/internal/utils/imgutils"
 )
 
 func TestStepRescaleImage_PerformExec(t *testing.T) {
 	imgFixtures := ([2]image.Image)(
-		imgcompare.ImageFixtures(2, []byte("TestStepRescaleImage_PerformExec")),
+		imgutils.ImageFixtures(2, []byte("TestStepRescaleImage_PerformExec")),
 	)
 	testCases := []struct {
 		name                  string
@@ -103,7 +103,7 @@ func TestStepRescaleImage_PerformExec(t *testing.T) {
 			}
 
 			result := state.img
-			if !imgcompare.IsImageEqual(result, tCase.expectedImg) {
+			if !imgutils.IsImageEqual(result, tCase.expectedImg) {
 				t.Errorf(
 					"expected: %#v, actual: %#v", tCase.expectedImg, result,
 				)
